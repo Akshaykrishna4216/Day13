b27583d46ab56f5bb6cb84746ffcc02b037860de
Akshaykrishna4216 <129931476+Akshaykrishna4216@users.noreply.github.com>
2023-05-02 17:02:14 +0530

    Add files via upload

---
+++ Anonymous/Anonymous/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymous
+{
+    internal class Program
+    {
+        public delegate int refmethod(int a, int b);
+        
+        static void Main(string[] args)
+        {
+            int result;
+            refmethod add = delegate (int i, int j)
+            {
+                return i + j;
+            };
+           result=add(1, 2);
+            Console.WriteLine(result);
+            
+        }
+    }
+}
